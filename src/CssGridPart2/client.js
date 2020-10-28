ca5d2a7d07d5ee5db6f9958e01a8e2705bfc9403
buingoctai <buingoctai994@gmail.com>
2020-10-28 22:23:00 +0700

    send build state to client, update lasted process id

--- src/CssGridPart2/client.js
+++ src/CssGridPart2/client.js
@@ -4,48 +4,49 @@ class RunBuild {
   constructor() {
     this.socket = null;
   }
-  doBuild(params) {
+  doBuild({ params, buildEvents }) {
     this.socket = new WebSocket("ws://localhost:80", ["json", "xml"]);
     this.socket.addEventListener("open", () => { // đã kết nối thành  công và sẵn sàng cho giao tiếp
       console.log("params=", params);
       this.socket.send(JSON.stringify(params));
     });
+    const { onProcessingBuild, onFinishedBuild, onError, onCanceledBuild } = buildEvents;
+    this.socket.addEventListener("message", (event) => {
+      const { isCanceled, isError, isFinished, message, code, buildId } = JSON.parse(event.data);
 
-    return new Promise((resolve, reject) => {
-      // Fire message event when incomming message
-      this.socket.addEventListener("message", (event) => {
-        const { isCanceled, isError, message, ...restParams } = JSON.parse(event.data);
+      if (isCanceled) {
+        onCanceledBuild({ message, code });
+        this.onClose();
+        return;
+      };
+      if (isError) {
+        onError({ message, code });
+        this.onClose();
+        return;
+      }
 
-        if (isCanceled) return;
-        if (isError) {
-          return reject(message);
-        }
-        resolve({ message, ...restParams });
-      });
-      // Fire error event when connecting to websocket error
-      this.socket.addEventListener("error", (event) => {
-        console.log("event error=");
-        reject(event.message);
-      });
-      // Fire close event when call close() function or auto calling after error event
-      // Closing and closed
-      this.socket.addEventListener("close", (event) => {
-        console.log("Close ket noi");
-      });
+      if (isFinished) {
+        onFinishedBuild({ message, code });
+        this.onClose();
+      } else {
+        onProcessingBuild({ message, code, buildId });
+      }
+    });
+    // Fire error event when connecting to websocket error
+    this.socket.addEventListener("error", (event) => {
+      const code = event.message ? 'ERROR_CONNECT' : 'ERROR_SERVER_CRASH';
+      onError({ message: event.message, code });
+      this.onClose();
+    });
+    // Fire close event when call close() function or auto calling after error event
+    // Closing and closed
+    this.socket.addEventListener("close", (event) => {
+      console.log("Connection was closed");
     });
   }
   cancelBuild(params) {
     if (!this.socket) return;
     this.socket.send(JSON.stringify(params));
-    return new Promise((resolve, reject) => {
-      this.socket.addEventListener("message", (event) => {
-        const { isError, message } = JSON.parse(event.data);
-        if (isError) {
-          return reject(message);
-        }
-        resolve(message);
-      });
-    });
   }
   onClose() {
     if (!this.socket) return;
--- src/CssGridPart2/index.js
+++ src/CssGridPart2/index.js
@@ -4,7 +4,24 @@ import backgroud from "./backgroud.jpg";
 // import { doRunBuild, cancelRunBuild } from "./client";
 import RunBuild from "./client";
 
-let newPid;
+let newBuildId;
+const BUILD_MESSAGE = {
+  START_BUILD: 'Start run build',
+  CLONE_SOURCE: 'Start clone zalo pc code',
+  CHECKOUT_AND_BUILD: 'Start checkout branch, build app',
+  UPLOAD_FILE: 'Start upload file',
+
+  TIMEOUT: 'Websocket timed out',
+
+  CLOSE_CONNECT: 'Websocket connection was closed by the client',
+  CANCEL_SUCCESS: 'Build Processing was success cancel',
+  BUILD_SUCCESS: 'Build was success',
+
+  /* errors */
+  ERROR_CONNECT: 'Websocket connection was error',
+  ERROR_BUILDER: 'There are errors on running builder',
+  ERROR_SERVER_CRASH: 'The connection was turned off by server',
+};
 const CssGridPart2 = () => {
   // const msg = new Msg({
   //   decryptKey: 'decryptKey',
@@ -15,58 +32,49 @@ const CssGridPart2 = () => {
 
   const builder = new RunBuild();
   const onRunBuild = () => {
-    console.log("on click");
+    console.log("on click do build");
     const params = {
       buildType: "buildBranch",
-      buildInfor: { buildBranch: "features/release_352" },
+      buildInfor: { buildBranch: "features/release_353" },
       token: 'R3YKZFKBVi',
     };
-
     // const params = {
     //   buildType: "buildTag",
     //   buildInfor: { buildTagVersion: "V101", buildTagType: "new feature" },
     // };
-    builder.doBuild(params)
-      .then((res) => {
-        console.log("res", res);
-        const { isFinished, message, pid } = res;
-        newPid = pid;
-        if (isFinished) {
-          builder.onClose();
-          console.log(message); // Get download url
-        } else {
-          console.log(message);  // Get build state
-        }
-      })
-      .catch((err) => {
-        builder.onClose();
-        console.log("err xxx", err);
-      });
-  }
-  const onCancelBuild = () => {
-    const params = {
-      // pid: newPid,
-      pid: 2002
-    }
-    builder.cancelBuild(params)
-      .then((res) => {
-        // Log success cancel noti
-        builder.onClose();
-        console.log(res);
-      })
-      .catch((err) => {
-        // Log not success cancel noti
-        console.log(err);
-      });
 
+    builder.doBuild({ params, buildEvents });
   }
+  const onRunCancel = () => {
+    const params = {
+      buildId: newBuildId,
+    }
+    builder.cancelBuild(params);
+  }
+  //----------------
+  const onProcessingBuild = (params) => {
+    if (params.buildId) newBuildId = params.buildId;
+
+    console.log("onProcessingBuild: ", BUILD_MESSAGE[params.code]);
+  }
+  const onFinishedBuild = (params) => {
+    console.log("onFinishedBuild: ", BUILD_MESSAGE[params.code]);
+  }
+  const onCanceledBuild = (params) => {
+    console.log("onCanceledBuild: ", BUILD_MESSAGE[params.code]);
+  }
+  const onError = (params) => {
+    console.log("onError: ", BUILD_MESSAGE[params.code]);
+  }
+  const buildEvents = { onProcessingBuild, onFinishedBuild, onError, onCanceledBuild };
+
 
   return <div className="wrapper">
     <h1>some heading</h1>
     <p>some content and stuff</p>
     <img className="full__bleed" alt="backgroud" src={backgroud} />
     <button onClick={onRunBuild}>do run build</button>
-    <button onClick={onCancelBuild}>cancel run build</button>
+    <button onClick={onRunCancel}>cancel run build</button>
   </div>
 };
 
